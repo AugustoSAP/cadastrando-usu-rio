4736976e34a1ebd35b25da96b76ca1aa71f60d00
Augusto <v493707@gmail.com>
2024-08-24 10:21:16 -0300

    Nova mensagem de commit

---
+++ server.js
@@ -0,0 +1,76 @@
+import express from "express";
+import { PrismaClient } from '@prisma/client';
+import cors from 'cors'
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.get('/usuarios', async (req, res) => {
+    try {
+        const users = await prisma.user.findMany()
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json({ error: "Erro ao buscar usuários." })
+    }
+})
+
+app.post('/usuarios', async (req, res) => {
+    try {
+        const user = await prisma.user.create({
+            data: {
+                email: req.body.email,
+                age: req.body.age,
+                name: req.body.name
+            }
+        })
+        res.status(201).json(user)
+    } catch (error) {
+        res.status(500).json({ error: "Erro ao criar usuário." })
+    }
+})
+
+app.put('/usuarios/:id', async (req, res) => {
+    try {
+        const userId = parseInt(req.params.id, 10); // Conversão de ID para inteiro
+        const user = await prisma.user.update({
+            where: { id: userId },
+            data: {
+                email: req.body.email,
+                age: req.body.age,
+                name: req.body.name
+            }
+        })
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({ error: "Erro ao atualizar usuário." })
+    }
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+    try {
+        const userId = parseInt(req.params.id, 10); // Conversão de ID para inteiro
+        await prisma.user.delete({
+            where: { id: userId }
+        })
+        res.status(200).json({ message: "Usuário deletado com sucesso!" })
+    } catch (error) {
+        res.status(500).json({ error: "Erro ao deletar usuário." })
+    }
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000")
+})
+
+
+//acesso a o BD
+
+//augusto
+//E9TiXVEt2ubU6Ct
